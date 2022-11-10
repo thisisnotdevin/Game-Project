99e53222d4d038eaab7423d6c970e2222205120a
thisisnotdevin <devin1299@outlook.com>
2022-11-10 11:19:26 -0500

    added score

---
+++ Fall 2022 Lecture/Assets/SampleScenes/Scripts/BallonGrowth.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallonGrowth : MonoBehaviour
+{
+    public float timer = 0f;
+    public float growTime = 6f;
+    public float maxSize = 2f;
+    public bool isMaxSize = false;
+    public GameObject deathEffect;
+
+    void Start()
+    {
+       if(isMaxSize == false){
+           StartCoroutine(Grow());
+       } 
+    }
+
+    private IEnumerator Grow()
+    {
+        Vector2 startScale = transform.localScale;
+        Vector2 maxScale = new Vector2(maxSize, maxSize);
+
+        do
+        {
+            //Grow, increment timer
+            transform.localScale = Vector3.Lerp(startScale, maxScale, timer / growTime);
+            timer += Time.deltaTime;
+            yield return null;
+
+        } while (timer < growTime);
+
+        isMaxSize = true;
+        if(isMaxSize == true)
+        {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+         
+        }
+    }
+
+   
+
+}
---
+++ Fall 2022 Lecture/Assets/SampleScenes/Scripts/Friendly.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Friendly : MonoBehaviour
+{
+    
+    public int health = 50;
+
+    public GameObject deathEffect;
+    public AudioSource audioPlayer;
+    // Start is called before the first frame update
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0){
+            
+            Die();
+        }
+    }
+
+    // Update is called once per frame
+    void Die ()
+    {
+     ScoreScript.scoreValue -= 10;
+     Instantiate(deathEffect, transform.position, Quaternion.identity);
+     Destroy(gameObject);
+     audioPlayer.Play();
+    }
+}
