49fd29437b2d649f3f91b29ada94b5196c7be903
thisisnotdevin <devin1299@outlook.com>
2022-11-10 21:21:58 -0500

    added scene management

--- Fall 2022 Lecture/Assets/SampleScenes/Scripts/ScoreScript.cs
+++ Fall 2022 Lecture/Assets/SampleScenes/Scripts/ScoreScript.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 
 
 public class ScoreScript : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public static int scoreValue;
+    private int nextScene;
    
 
 
     void Start()
     {
         scoreText = GetComponent<TextMeshProUGUI>();
-       
+       nextScene = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = "Score: " + scoreValue;
-       
+       if(scoreValue == 100)
+       {
+           SceneManager.LoadScene(nextScene);
+       }
     }
 }
