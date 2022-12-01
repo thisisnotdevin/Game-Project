5223c2898fe0b17068fb18d6f2fca5581ccee8f1
thisisnotdevin <devin1299@outlook.com>
2022-12-01 13:03:08 -0500

    fixed score, added impact

--- Fall 2022 Lecture/Assets/SampleScenes/Scripts/EnemySpawner.cs
+++ Fall 2022 Lecture/Assets/SampleScenes/Scripts/EnemySpawner.cs
@@ -12,7 +12,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private float swarmerInterval = 3.5f;
     [SerializeField]
-    private float bigSwarmerInterval = 10f;
+    private float bigSwarmerInterval = 8f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +24,7 @@ public class EnemySpawner : MonoBehaviour
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-4f, 4), Random.Range(-5f, 5f), 0), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
